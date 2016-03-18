a51b891bf91e9c34dd72184301f2c3ab1cca8c39
Bhargav Krishna <bhargav.krishna@outlook.com>
2016-03-18 15:14:35 -0700

    Refactoring the code and renaming files for semantics

--- FunctionFile/authorize.js
+++ FunctionFile/authorize.js
@@ -1,28 +1,35 @@
 $(document).ready(function() {
     var token = getToken(window.document.URL);
-    $('#token').text('THe token is:' + JSON.stringify(token));
-    window.localStorage.setItem('accessToken', JSON.stringify(token));
-});
+    if (token !== null) {
+        window.localStorage.setItem('accessToken', JSON.stringify(token));
+        $('#success').show();
+        $('#failed').hide();
+    }
+    else {
+        $('#success').hide();
+        $('#failed').show();
+    }
 
-function getToken(hash) {
-    var parts = hash.split('?');
-    if (parts && parts.length > 0) {
+    function getToken(hash) {
+        var parts = hash.split('?');
+        if (parts == null || parts.length <= 0) return null;
+
         var rightPart = parts.length == 2 ? parts[1] : parts[0];
         var token = getTokenFromString(rightPart);
         return token;
     }
 
-    return '';
-}
+    function getTokenFromString(hash) {
+        var params = {},
+            regex = /([^&=]+)=([^&]*)/g;
 
-function getTokenFromString(hash) {
-    let params = {},
-        regex = /([^&=]+)=([^&]*)/g,
-        m;
+        var matches = regex.exec(hash);
+        if (matches == null) return null;
 
-    while ((m = regex.exec(hash)) !== null) {
-        params[decodeURIComponent(m[1])] = decodeURIComponent(m[2]);
+        for (var i = matches.length - 1; i >= 0; i--) {
+            params[decodeURIComponent(matches[i])] = decodeURIComponent(matches[i]);
+        }
+
+        return params;
     }
-
-    return params;
-}
+});
